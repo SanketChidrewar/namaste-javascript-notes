a3797fe89043637989683573702a0d88722bb519
Chidrewar Sanket <59963910+SanketChidrewar@users.noreply.github.com>
2024-03-30 05:34:10 +0000

    prototype example for var, fun & array

---
+++ js examples/prototype-prototypal_inheritance.js
@@ -0,0 +1,13 @@
+let sum = (a, b) => a + b;
+
+let emp = {
+    name: "Sanket",
+    age: 26,
+    city: "Pune"
+}
+
+let arr = [2, 3, 5, 8];
+
+console.log(sum.__proto__);
+console.log(emp.__proto__);
+console.log(arr.__proto__);
