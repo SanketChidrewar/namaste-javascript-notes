e810b905a15e882079130faf1770bccfcbda4606
Chidrewar Sanket <59963910+SanketChidrewar@users.noreply.github.com>
2024-03-30 05:39:19 +0000

    prototype can cause performace issue if we don't use it wisely

--- js examples/prototype-prototypal_inheritance.js
+++ js examples/prototype-prototypal_inheritance.js
@@ -11,3 +11,25 @@ let arr = [2, 3, 5, 8];
 console.log(sum.__proto__);
 console.log(emp.__proto__);
 console.log(arr.__proto__);
+
+
+// don't do this, it is just for demonstration (it will lead to huge performance issue).
+let obj1 = {
+    name: "Sanket",
+    age: 26,
+    city: "Pune",
+    getDetails: function(){
+        console.log(this.name + " is from " + this.city);
+    }
+}
+
+let obj2 = {
+    name: "Aditya"
+}
+
+obj2.__proto__ = obj1;
+
+obj1.getDetails();
+
+console.log(obj2.__proto__);
+obj2.getDetails(); //here obj2 prototype value is attached as obje 1 function.
